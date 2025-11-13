9f020c49176dffa362cdc4081b597277912b3e6c
naufal syuja <m.naufal.s.k@gmail.com>
2025-11-14 01:22:25 +0700

    [feat] : add warta in home_page.dart

--- lib/features/home/presentation/views/home_page.dart
+++ lib/features/home/presentation/views/home_page.dart
@@ -7,11 +7,14 @@ import 'package:hijauin_frontend_mobile/common/primary_text.dart';
 import 'package:hijauin_frontend_mobile/features/home/presentation/components/aqi_home_shimmer.dart';
 import 'package:hijauin_frontend_mobile/features/home/presentation/components/aqi_home_widget.dart';
 import 'package:hijauin_frontend_mobile/features/home/presentation/components/item_home_widget.dart';
-import 'package:hijauin_frontend_mobile/features/home/presentation/components/item_warta_home.dart';
 import 'package:hijauin_frontend_mobile/features/home/presentation/cubit/aqi_home/aqi_widget_cubit.dart';
 import 'package:hijauin_frontend_mobile/features/main/cubit/main_page_cubit.dart';
+import 'package:hijauin_frontend_mobile/features/warta/data/services/services.dart';
+import 'package:hijauin_frontend_mobile/features/warta/presentation/components/item_warta.dart';
+import 'package:hijauin_frontend_mobile/features/warta/presentation/cubit/warta_list/warta_list_cubit.dart';
 import 'package:hijauin_frontend_mobile/utils/location_service.dart';
 import 'package:hijauin_frontend_mobile/utils/shared_storage.dart';
+import 'package:hijauin_frontend_mobile/utils/shimmer_card.dart';
 
 class HomePage extends StatefulWidget {
   static const String routeName = "home-page";
@@ -74,17 +77,19 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: const Color(0xFFF5F5F5),
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage(Constants.imgBgAuth))),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
+    return BlocProvider(
+      create: (context) => WartaListCubit(WartaServices())..fetchArticles(page: 1, limit: 4),
+      child: Scaffold(
+          backgroundColor: const Color(0xFFF5F5F5),
+          body: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage(Constants.imgBgAuth))),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
                 Container(
                   width: double.infinity,
                   decoration: ShapeDecoration(
@@ -228,35 +233,129 @@ class _HomePageState extends State<HomePage> {
                           )
                         ],
                       ),
-                      PrimaryText(
-                        text: "Lihat Semua",
-                        color: colorTextPrimary,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        lineHeight: 1.43,
+                      GestureDetector(
+                        onTap: () {
+                          context.read<MainPageCubit>().setPage(4);
+                        },
+                        child: PrimaryText(
+                          text: "Lihat Semua",
+                          color: colorTextPrimary,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          lineHeight: 1.43,
+                        ),
                       )
                     ],
                   ),
                 ),
+                SizedBox(height: 16),
                 Padding(
                   padding:
                       const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-                  child: GridView.builder(
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          mainAxisSpacing: 20,
-                          crossAxisSpacing: 20,
-                          childAspectRatio: 0.8),
-                      itemCount: 3,
-                      itemBuilder: (context, index) {
-                        return ItemWartaHome();
-                      }),
+                  child: BlocBuilder<WartaListCubit, WartaListState>(
+                    builder: (context, state) {
+                      if (state is WartaListLoading) {
+                        return GridView.builder(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 20,
+                            crossAxisSpacing: 20,
+                            childAspectRatio: 0.8,
+                          ),
+                          itemCount: 4,
+                          itemBuilder: (context, index) {
+                            return ShimmerCard(
+                              width: 187,
+                              height: 250,
+                              radius: 12,
+                            );
+                          },
+                        );
+                      }
+
+                      if (state is WartaListLoaded) {
+                        final articles = state.articles.take(4).toList();
+                        
+                        if (articles.isEmpty) {
+                          return Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(32.0),
+                              child: PrimaryText(
+                                text: 'Belum ada artikel',
+                                fontSize: 14,
+                                color: Color(0xFF6B7280),
+                              ),
+                            ),
+                          );
+                        }
+
+                        return GridView.builder(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 20,
+                            crossAxisSpacing: 20,
+                            childAspectRatio: 0.8,
+                          ),
+                          itemCount: articles.length,
+                          itemBuilder: (context, index) {
+                            return ItemWartaHome(article: articles[index]);
+                          },
+                        );
+                      }
+
+                      if (state is WartaListError) {
+                        return Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(32.0),
+                            child: Column(
+                              children: [
+                                Icon(
+                                  Icons.error_outline,
+                                  size: 48,
+                                  color: Color(0xFF9CA3AF),
+                                ),
+                                SizedBox(height: 12),
+                                PrimaryText(
+                                  text: 'Gagal memuat artikel',
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color(0xFF111827),
+                                ),
+                                SizedBox(height: 8),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    context.read<WartaListCubit>().fetchArticles(page: 1, limit: 4);
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Color(0xFF2D746A),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                  ),
+                                  child: PrimaryText(
+                                    text: 'Coba Lagi',
+                                    fontSize: 12,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }
+
+                      return SizedBox.shrink();
+                    },
+                  ),
                 ),
               ],
             ),
           ),
-        ));
+        )),
+    );
   }
 }
